bdb63cd7a5f43f128a09e8644dd94bc2ecda60a3
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-23 16:36:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,10 +1,10 @@
 require "pry"
 def find_item_by_name_in_collection(name, collection)
-collection.each do |item|
-  if collection[:item] == name
-    return collection[:item]
-  end 
-  end 
+ collection.each do |name|
+   if name[:item] == name 
+     return name[:item]
+   end 
+ end 
 end
 
 def consolidate_cart(cart)
