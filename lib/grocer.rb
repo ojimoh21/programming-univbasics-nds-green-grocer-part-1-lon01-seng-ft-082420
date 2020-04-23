a5cc6682006a76534391b6a37ab2eec329c91e47
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-23 17:12:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,13 +1,13 @@
 require "pry"
 
 
-def find_item_by_name_in_collection(name, collection)
- collection.each do |items|
-   if items[:item] == name
-     return items
-   end 
- end 
-end 
+# def find_item_by_name_in_collection(name, collection)
+# collection.each do |items|
+#   if items[:item] == name
+#     return items
+#   end 
+# end 
+# end 
 
 
 
