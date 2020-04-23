b71f6b69f8dee94ee4e8fa2f643e6e1987cd5b8b
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-23 16:14:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,11 +1,8 @@
 require "pry"
 def find_item_by_name_in_collection(name, collection)
-  collection.each do |item|
-    if name == item
-      return item
-      binding.pry
-    end 
-  end 
+collection.each do |items, name|
+  binding.pry
+end 
 end
 
 def consolidate_cart(cart)
@@ -17,4 +14,12 @@ def consolidate_cart(cart)
 end
 
 
-  
+  def num_points_scored(player_name)
+game_hash.each do |team, team_info|
+  team_info[:players].each do |player|
+    if player[:player_name] == player_name
+      return player[:points]
+      end 
+    end 
+  end 
+end
